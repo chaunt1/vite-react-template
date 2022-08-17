dea99b2e677bd5e9bdda869789614216234c87a7
chaunt <chau.nguyentuong@abs.vn>
2022-08-17 15:29:50 +0700

    feat: init i18n

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     port: 8080,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss', '.json'],
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      { find: '@config', replacement: path.resolve(__dirname, 'src/config') },
+      { find: '@contants', replacement: path.resolve(__dirname, 'src/contants') },
+      { find: '@locales', replacement: path.resolve(__dirname, 'src/locales') },
+      { find: '@redux', replacement: path.resolve(__dirname, 'src/redux') },
+      { find: '@services', replacement: path.resolve(__dirname, 'src/services') },
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+    ],
   },
   build: {
     sourcemap: false,
